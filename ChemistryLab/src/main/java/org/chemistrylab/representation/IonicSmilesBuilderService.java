963a19453bbb9ee9b45df579b7610dcc891f5872
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-07 01:39:12 +0200

    Reuse shared oxoanion smiles catalog in ionic builder

--- ChemistryLab/src/main/java/org/chemistrylab/representation/IonicSmilesBuilderService.java
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/IonicSmilesBuilderService.java
@@ -8,37 +8,12 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class IonicSmilesBuilderService {
 
     private final IonicFormulaResolver ionicFormulaResolver;
-
-    private static final Map<String, String> OXOANION_SMILES = Map.ofEntries(
-            Map.entry("NO3", "[O-][N+](=O)[O-]"),
-            Map.entry("NO2", "O=[N+][O-]"),
-            Map.entry("SO4", "[O-]S(=O)(=O)[O-]"),
-            Map.entry("SO3", "[O-]S(=O)[O-]"),
-            Map.entry("HSO4", "OS(=O)(=O)[O-]"),
-            Map.entry("HSO3", "OS(=O)[O-]"),
-            Map.entry("CO3", "[O-]C(=O)[O-]"),
-            Map.entry("HCO3", "OC(=O)[O-]"),
-            Map.entry("PO4", "[O-]P(=O)([O-])[O-]"),
-            Map.entry("HPO4", "OP(=O)([O-])[O-]"),
-            Map.entry("H2PO4", "OP(=O)(O)[O-]"),
-            Map.entry("ClO", "[O-]Cl"),
-            Map.entry("ClO2", "O=Cl[O-]"),
-            Map.entry("ClO3", "O=Cl(=O)[O-]"),
-            Map.entry("ClO4", "O=Cl(=O)(=O)[O-]"),
-            Map.entry("BrO3", "O=Br(=O)[O-]"),
-            Map.entry("IO3", "O=I(=O)[O-]"),
-            Map.entry("MnO4", "[O-][Mn](=O)(=O)=O"),
-            Map.entry("CrO4", "[O-][Cr](=O)(=O)[O-]"),
-            Map.entry("Cr2O7", "[O-][Cr](=O)(=O)O[Cr](=O)(=O)[O-]"),
-            Map.entry("Fe(CN)6", "N#C[Fe](C#N)(C#N)(C#N)(C#N)C#N")
-    );
 
     public IonicSmilesBuilderService(IonicFormulaResolver ionicFormulaResolver) {
         this.ionicFormulaResolver = ionicFormulaResolver;
@@ -65,11 +40,12 @@ public class IonicSmilesBuilderService {
             return compact;
         }
 
-        String anionSmiles = OXOANION_SMILES.get(anion.getFormula());
-        if (anionSmiles == null || anionSmiles.isBlank()) {
+        Optional<String> anionSmilesOptional = EducationalOxoanionSmilesCatalog.find(anion.getFormula());
+        if (anionSmilesOptional.isEmpty()) {
             return Optional.empty();
         }
 
+        String anionSmiles = anionSmilesOptional.get();
         List<String> fragments = new ArrayList<>();
         fragments.add(anionSmiles);
 
@@ -89,7 +65,6 @@ public class IonicSmilesBuilderService {
         IonConfig cation = cationMatch.getIon();
         IonConfig anion = anionMatch.getIon();
 
-        String cationFormula = cation.getFormula();
         String anionFormula = anion.getFormula();
 
         if (isMonoatomic(anionFormula)) {
