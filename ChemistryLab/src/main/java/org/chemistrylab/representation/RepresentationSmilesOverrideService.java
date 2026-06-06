33811dc5189e31d94a98f9f5bf9e11a44cb93db5
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-06 20:36:49 +0200

    Add formula aliases for curated representation overrides

--- ChemistryLab/src/main/java/org/chemistrylab/representation/RepresentationSmilesOverrideService.java
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/RepresentationSmilesOverrideService.java
@@ -13,22 +13,39 @@ public class RepresentationSmilesOverrideService {
 
     private static final Map<String, String> OVERRIDES = Map.ofEntries(
             Map.entry("SO3", "O=S(=O)=O"),
+            Map.entry("O3S", "O=S(=O)=O"),
             Map.entry("NO", "N=O"),
+            Map.entry("ON", "N=O"),
             Map.entry("SO2", "O=S=O"),
+            Map.entry("O2S", "O=S=O"),
             Map.entry("NO2", "O=[N+][O-]"),
+            Map.entry("O2N", "O=[N+][O-]"),
             Map.entry("N2O", "N#[N+]O"),
+            Map.entry("ON2", "N#[N+]O"),
             Map.entry("CO2", "O=C=O"),
+            Map.entry("O2C", "O=C=O"),
             Map.entry("CO", "[C-]#[O+]"),
+            Map.entry("OC", "[C-]#[O+]"),
             Map.entry("H2O", "O"),
+            Map.entry("OH2", "O"),
             Map.entry("NH3", "N"),
+            Map.entry("H3N", "N"),
             Map.entry("H2O2", "OO"),
+            Map.entry("O2H2", "OO"),
             Map.entry("CaO", "[Ca+2].[O-2]"),
+            Map.entry("OCa", "[Ca+2].[O-2]"),
             Map.entry("MgO", "[Mg+2].[O-2]"),
+            Map.entry("OMg", "[Mg+2].[O-2]"),
             Map.entry("FeO", "[Fe+2].[O-2]"),
+            Map.entry("OFe", "[Fe+2].[O-2]"),
             Map.entry("CuO", "[Cu+2].[O-2]"),
+            Map.entry("OCu", "[Cu+2].[O-2]"),
             Map.entry("ZnO", "[Zn+2].[O-2]"),
+            Map.entry("OZn", "[Zn+2].[O-2]"),
             Map.entry("Al2O3", "[Al+3].[Al+3].[O-2].[O-2].[O-2]"),
-            Map.entry("Fe2O3", "[Fe+3].[Fe+3].[O-2].[O-2].[O-2]")
+            Map.entry("O3Al2", "[Al+3].[Al+3].[O-2].[O-2].[O-2]"),
+            Map.entry("Fe2O3", "[Fe+3].[Fe+3].[O-2].[O-2].[O-2]"),
+            Map.entry("O3Fe2", "[Fe+3].[Fe+3].[O-2].[O-2].[O-2]")
     );
 
     public RepresentationSmilesOverrideService(FormulaParserService formulaParserService) {
