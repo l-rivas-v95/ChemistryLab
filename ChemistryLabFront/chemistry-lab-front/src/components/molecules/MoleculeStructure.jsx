416344f7b43599d76400d6c40ce287dbecc99998
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-06 21:28:13 +0200

    Render isolated SVG molecule representations

--- ChemistryLabFront/chemistry-lab-front/src/components/molecules/MoleculeStructure.jsx
+++ ChemistryLabFront/chemistry-lab-front/src/components/molecules/MoleculeStructure.jsx
@@ -10,6 +10,15 @@ function MoleculeStructure({ molecula }) {
         return <FormulaStructure formula={molecula?.formula} />;
     }
 
+    if (representacion.tipoRepresentacion === "SVG" && representacion.svg) {
+        return (
+            <SvgStructure
+                svg={representacion.svg}
+                formula={representacion.formulaVisual || molecula?.formula}
+            />
+        );
+    }
+
     if (representacion.tipoRepresentacion === "IMAGEN_2D" && representacion.imagen2d) {
         return (
             <ExternalImageStructure
@@ -21,6 +30,19 @@ function MoleculeStructure({ molecula }) {
     }
 
     return <FormulaStructure formula={representacion.formulaVisual || molecula?.formula} />;
+}
+
+function SvgStructure({ svg, formula }) {
+    if (!svg) {
+        return <FormulaStructure formula={formula} />;
+    }
+
+    return (
+        <div
+            className="formula-structure formula-structure-html molecule-svg-structure-html"
+            dangerouslySetInnerHTML={{ __html: svg }}
+        />
+    );
 }
 
 function ExternalImageStructure({ src, alt, formula }) {
