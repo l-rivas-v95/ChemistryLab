667734c7786495d892829cb13341842e2713442d
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-07 19:18:52 +0200

    fix: detach selected oxosalts from metals

--- ChemistryLab/src/main/java/org/chemistrylab/representation/RepresentationSmilesOverrideService.java
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/RepresentationSmilesOverrideService.java
@@ -118,52 +118,54 @@ public class RepresentationSmilesOverrideService {
     }
 
     private static void oxisalesCompactas(Map<String, String> overrides) {
-        put(overrides, "NaNO3", "[Na]O[N+](=O)[O-]");
-        put(overrides, "KNO3", "[K]O[N+](=O)[O-]");
-        put(overrides, "AgNO3", "[Ag]O[N+](=O)[O-]");
-        put(overrides, "Ca(NO3)2", "O=N(=O)O[Ca]O[N+](=O)[O-]");
-        put(overrides, "Cu(NO3)2", "O=N(=O)O[Cu]O[N+](=O)[O-]");
-        put(overrides, "Fe(NO3)3", "O=N(=O)O[Fe](O[N+](=O)[O-])O[N+](=O)[O-]");
+        put(overrides, "NaNO3", "[O-][N+](=O)[O-].[Na+]", "NNaO3");
+        put(overrides, "KNO3", "[O-][N+](=O)[O-].[K+]", "KNO3");
+        put(overrides, "AgNO3", "[O-][N+](=O)[O-].[Ag+]", "AgNO3");
+        put(overrides, "Ca(NO3)2", "[O-][N+](=O)[O-].[Ca+2].[O-][N+](=O)[O-]");
+        put(overrides, "Cu(NO3)2", "[O-][N+](=O)[O-].[Cu+2].[O-][N+](=O)[O-]");
+        put(overrides, "Fe(NO3)3", "[O-][N+](=O)[O-].[O-][N+](=O)[O-].[Fe+3].[O-][N+](=O)[O-]");
 
-        put(overrides, "NaNO2", "[Na]ON=O");
-        put(overrides, "KNO2", "[K]ON=O");
+        put(overrides, "NaNO2", "[O-]N=O.[Na+]", "NNaO2");
+        put(overrides, "KNO2", "[O-]N=O.[K+]", "KNO2");
+        put(overrides, "Ca(NO2)2", "[O-]N=O.[Ca+2].[O-]N=O");
 
-        put(overrides, "Na2CO3", "[Na]OC(=O)O[Na]");
-        put(overrides, "K2CO3", "[K]OC(=O)O[K]");
-        put(overrides, "MgCO3", "O=C(O[Mg])O");
-        put(overrides, "CaCO3", "O=C(O[Ca])O");
-        put(overrides, "KHCO3", "[K]OC(=O)O[H]");
-        put(overrides, "NaHCO3", "[Na]OC(=O)O[H]");
+        put(overrides, "Na2CO3", "[Na+].[O-]C(=O)[O-].[Na+]", "CNa2O3");
+        put(overrides, "K2CO3", "[K+].[O-]C(=O)[O-].[K+]", "CK2O3");
+        put(overrides, "MgCO3", "[O-]C(=O)[O-].[Mg+2]", "CMgO3");
+        put(overrides, "CaCO3", "[O-]C(=O)[O-].[Ca+2]", "CCaO3");
+        put(overrides, "KHCO3", "OC(=O)[O-].[K+]", "CHKO3");
+        put(overrides, "NaHCO3", "OC(=O)[O-].[Na+]", "CHNaO3");
+        put(overrides, "Ca(HCO3)2", "OC(=O)[O-].[Ca+2].OC(=O)[O-]", "CaH2C2O6");
 
-        put(overrides, "Na2SO4", "[Na]OS(=O)(=O)O[Na]");
-        put(overrides, "K2SO4", "[K]OS(=O)(=O)O[K]");
-        put(overrides, "CaSO4", "O=S(=O)(O[Ca])O");
-        put(overrides, "MgSO4", "O=S(=O)(O[Mg])O");
-        put(overrides, "CuSO4", "O=S(=O)(O[Cu])O");
-        put(overrides, "ZnSO4", "O=S(=O)(O[Zn])O");
-        put(overrides, "FeSO4", "O=S(=O)(O[Fe])O");
-        put(overrides, "Al2(SO4)3", "O=S(=O)(O[Al]OS(=O)(=O)O[Al]OS(=O)(=O)O)O");
+        put(overrides, "Na2SO4", "[Na+].[O-]S(=O)(=O)[O-].[Na+]", "Na2O4S");
+        put(overrides, "K2SO4", "[K+].[O-]S(=O)(=O)[O-].[K+]", "K2O4S");
+        put(overrides, "CaSO4", "[O-]S(=O)(=O)[O-].[Ca+2]", "CaO4S");
+        put(overrides, "MgSO4", "[O-]S(=O)(=O)[O-].[Mg+2]", "MgO4S");
+        put(overrides, "CuSO4", "[O-]S(=O)(=O)[O-].[Cu+2]", "CuO4S");
+        put(overrides, "ZnSO4", "[O-]S(=O)(=O)[O-].[Zn+2]", "O4SZn");
+        put(overrides, "FeSO4", "[O-]S(=O)(=O)[O-].[Fe+2]", "FeO4S");
+        put(overrides, "Al2(SO4)3", "[O-]S(=O)(=O)[O-].[Al+3].[O-]S(=O)(=O)[O-].[Al+3].[O-]S(=O)(=O)[O-]");
         put(overrides, "(NH4)2SO4", AMMONIUM + "." + AMMONIUM + ".[O-]S(=O)(=O)[O-]", "H8N2O4S");
 
-        put(overrides, "Na3PO4", "[Na]OP(=O)(O[Na])O[Na]");
-        put(overrides, "K3PO4", "[K]OP(=O)(O[K])O[K]");
-        put(overrides, "Ca3(PO4)2", "O=P(O[Ca]OP(=O)(O[Ca])O[Ca])O");
-        put(overrides, "Mg3(PO4)2", "O=P(O[Mg]OP(=O)(O[Mg])O[Mg])O");
-        put(overrides, "AlPO4", "O=P(O[Al])(O)O");
-        put(overrides, "FePO4", "O=P(O[Fe])(O)O");
-        put(overrides, "NaH2PO4", "[Na]OP(=O)(O[H])O[H]");
-        put(overrides, "Na2HPO4", "[Na]OP(=O)(O[Na])O[H]");
-        put(overrides, "CaHPO4", "O=P(O[Ca])(O[H])O");
+        put(overrides, "Na3PO4", "[Na+].[O-]P(=O)([O-])[O-].[Na+].[Na+]", "Na3O4P");
+        put(overrides, "K3PO4", "[K+].[O-]P(=O)([O-])[O-].[K+].[K+]", "K3O4P");
+        put(overrides, "Ca3(PO4)2", "[Ca+2].[O-]P(=O)([O-])[O-].[Ca+2].[O-]P(=O)([O-])[O-].[Ca+2]");
+        put(overrides, "Mg3(PO4)2", "[Mg+2].[O-]P(=O)([O-])[O-].[Mg+2].[O-]P(=O)([O-])[O-].[Mg+2]");
+        put(overrides, "AlPO4", "[O-]P(=O)([O-])[O-].[Al+3]", "AlO4P");
+        put(overrides, "FePO4", "[O-]P(=O)([O-])[O-].[Fe+3]", "FeO4P");
+        put(overrides, "NaH2PO4", "OP(=O)(O)[O-].[Na+]", "H2NaO4P");
+        put(overrides, "Na2HPO4", "OP(=O)([O-])[O-].[Na+].[Na+]", "HNa2O4P");
+        put(overrides, "CaHPO4", "OP(=O)([O-])[O-].[Ca+2]", "CaHO4P");
 
-        put(overrides, "NaClO", "[Na]OCl");
-        put(overrides, "NaClO2", "[Na]OCl=O");
-        put(overrides, "NaClO3", "[Na]OCl(=O)=O");
-        put(overrides, "KClO3", "[K]OCl(=O)=O");
-        put(overrides, "NaClO4", "[Na]OCl(=O)(=O)=O");
-        put(overrides, "KClO4", "[K]OCl(=O)(=O)=O");
-        put(overrides, "KMnO4", "[K]O[Mn](=O)(=O)=O");
-        put(overrides, "K2Cr2O7", "[K]O[Cr](=O)(=O)O[Cr](=O)(=O)O[K]");
-        put(overrides, "Na2Cr2O7", "[Na]O[Cr](=O)(=O)O[Cr](=O)(=O)O[Na]");
+        put(overrides, "NaClO", "Cl[O-].[Na+]", "ClNaO");
+        put(overrides, "NaClO2", "O=Cl[O-].[Na+]", "ClNaO2");
+        put(overrides, "NaClO3", "O=Cl(=O)[O-].[Na+]", "ClNaO3");
+        put(overrides, "KClO3", "O=Cl(=O)[O-].[K+]", "ClKO3");
+        put(overrides, "NaClO4", "O=Cl(=O)(=O)[O-].[Na+]", "ClNaO4");
+        put(overrides, "KClO4", "O=Cl(=O)(=O)[O-].[K+]", "ClKO4");
+        put(overrides, "KMnO4", "O=[Mn](=O)(=O)[O-].[K+]", "KMnO4");
+        put(overrides, "K2Cr2O7", "[K+].O=[Cr](=O)([O-])O[Cr](=O)(=O)[O-].[K+]", "Cr2K2O7");
+        put(overrides, "Na2Cr2O7", "[Na+].O=[Cr](=O)([O-])O[Cr](=O)(=O)[O-].[Na+]", "Cr2Na2O7");
     }
 
     private static void casosIonicLegacyUtiles(Map<String, String> overrides) {
