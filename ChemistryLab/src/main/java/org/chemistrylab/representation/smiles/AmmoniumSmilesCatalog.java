fa64844c1f06410870c4ed44f2731c0cd0e67c24
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-08 00:08:12 +0200

    refactor: use curated smiles registry for ammonium catalog

--- ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/AmmoniumSmilesCatalog.java
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/AmmoniumSmilesCatalog.java
@@ -2,7 +2,7 @@ package org.chemistrylab.representation.smiles;
 
 import java.util.Map;
 
-import static org.chemistrylab.representation.smiles.SmilesOverrideCatalog.put;
+import static org.chemistrylab.representation.smiles.CuratedSmilesRegistry.register;
 
 public final class AmmoniumSmilesCatalog {
 
@@ -11,26 +11,26 @@ public final class AmmoniumSmilesCatalog {
     private AmmoniumSmilesCatalog() {
     }
 
-    public static void register(Map<String, String> overrides) {
-        halides(overrides);
-        oxosalts(overrides);
-        hydroxide(overrides);
+    public static void register(Map<String, String> smilesByFormula) {
+        halides(smilesByFormula);
+        oxosalts(smilesByFormula);
+        hydroxide(smilesByFormula);
     }
 
-    private static void halides(Map<String, String> overrides) {
-        put(overrides, "NH4F", AMMONIUM + ".[F-]", "FH4N");
-        put(overrides, "NH4Cl", AMMONIUM + ".[Cl-]", "ClH4N");
-        put(overrides, "NH4Br", AMMONIUM + ".[Br-]", "BrH4N");
-        put(overrides, "NH4I", AMMONIUM + ".[I-]", "H4IN");
+    private static void halides(Map<String, String> smilesByFormula) {
+        register(smilesByFormula, "NH4F", AMMONIUM + ".[F-]", "FH4N");
+        register(smilesByFormula, "NH4Cl", AMMONIUM + ".[Cl-]", "ClH4N");
+        register(smilesByFormula, "NH4Br", AMMONIUM + ".[Br-]", "BrH4N");
+        register(smilesByFormula, "NH4I", AMMONIUM + ".[I-]", "H4IN");
     }
 
-    private static void oxosalts(Map<String, String> overrides) {
-        put(overrides, "NH4NO3", AMMONIUM + ".[O-][N+](=O)[O-]", "H4N2O3", "H4NNO3");
-        put(overrides, "(NH4)2SO4", AMMONIUM + "." + AMMONIUM + ".[O-]S(=O)(=O)[O-]", "H8N2O4S");
-        put(overrides, "(NH4)3PO4", AMMONIUM + "." + AMMONIUM + "." + AMMONIUM + ".[O-]P(=O)([O-])[O-]", "H12N3O4P");
+    private static void oxosalts(Map<String, String> smilesByFormula) {
+        register(smilesByFormula, "NH4NO3", AMMONIUM + ".[O-][N+](=O)[O-]", "H4N2O3", "H4NNO3");
+        register(smilesByFormula, "(NH4)2SO4", AMMONIUM + "." + AMMONIUM + ".[O-]S(=O)(=O)[O-]", "H8N2O4S");
+        register(smilesByFormula, "(NH4)3PO4", AMMONIUM + "." + AMMONIUM + "." + AMMONIUM + ".[O-]P(=O)([O-])[O-]", "H12N3O4P");
     }
 
-    private static void hydroxide(Map<String, String> overrides) {
-        put(overrides, "NH4OH", AMMONIUM + ".[OH-]", "H5NO");
+    private static void hydroxide(Map<String, String> smilesByFormula) {
+        register(smilesByFormula, "NH4OH", AMMONIUM + ".[OH-]", "H5NO");
     }
 }
