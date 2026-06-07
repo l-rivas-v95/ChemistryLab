37f706a81430874df729535c153f88392f904d05
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-07 04:12:50 +0200

    refactor: remove unused representation strategy

--- ChemistryLab/src/main/java/org/chemistrylab/representation/RepresentationStrategy.java
+++
@@ -1,9 +0,0 @@
-package org.chemistrylab.representation;
-
-public enum RepresentationStrategy {
-    DIRECT_SMILES,
-    INCHI_CONVERSION,
-    EDUCATIONAL_CANDIDATE,
-    SPECIAL_CASE,
-    FALLBACK
-}
