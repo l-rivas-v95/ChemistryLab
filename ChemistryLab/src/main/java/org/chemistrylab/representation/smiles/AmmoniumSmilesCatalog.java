d4a220000b5235c61d9b372da2537676cbd3b7f5
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-08 01:24:06 +0200

    Add common ammonium salt curated SMILES

--- ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/AmmoniumSmilesCatalog.java
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/AmmoniumSmilesCatalog.java
@@ -13,7 +13,9 @@ public final class AmmoniumSmilesCatalog {
 
     public static void register(Map<String, String> smilesByFormula) {
         halides(smilesByFormula);
+        simpleSalts(smilesByFormula);
         oxosalts(smilesByFormula);
+        acidOxosalts(smilesByFormula);
         hydroxide(smilesByFormula);
     }
 
@@ -24,10 +26,27 @@ public final class AmmoniumSmilesCatalog {
         add(smilesByFormula, "NH4I", AMMONIUM + ".[I-]", "H4IN");
     }
 
+    private static void simpleSalts(Map<String, String> smilesByFormula) {
+        add(smilesByFormula, "NH4CN", AMMONIUM + ".[C-]#N", "CH4N2");
+        add(smilesByFormula, "NH4SCN", AMMONIUM + ".[S-]C#N", "CH4N2S");
+    }
+
     private static void oxosalts(Map<String, String> smilesByFormula) {
         add(smilesByFormula, "NH4NO3", AMMONIUM + ".[O-][N+](=O)[O-]", "H4N2O3", "H4NNO3");
+        add(smilesByFormula, "NH4NO2", AMMONIUM + ".[O-]N=O", "H4N2O2");
+        add(smilesByFormula, "(NH4)2CO3", AMMONIUM + "." + AMMONIUM + ".[O-]C(=O)[O-]", "CH8N2O3");
+        add(smilesByFormula, "(NH4)2SO3", AMMONIUM + "." + AMMONIUM + ".[O-]S(=O)[O-]", "H8N2O3S");
         add(smilesByFormula, "(NH4)2SO4", AMMONIUM + "." + AMMONIUM + ".[O-]S(=O)(=O)[O-]", "H8N2O4S");
         add(smilesByFormula, "(NH4)3PO4", AMMONIUM + "." + AMMONIUM + "." + AMMONIUM + ".[O-]P(=O)([O-])[O-]", "H12N3O4P");
+        add(smilesByFormula, "NH4ClO3", AMMONIUM + ".O=Cl(=O)[O-]", "ClH4NO3");
+        add(smilesByFormula, "NH4ClO4", AMMONIUM + ".O=Cl(=O)(=O)[O-]", "ClH4NO4");
+    }
+
+    private static void acidOxosalts(Map<String, String> smilesByFormula) {
+        add(smilesByFormula, "NH4HCO3", AMMONIUM + ".OC(=O)[O-]", "CH5NO3");
+        add(smilesByFormula, "NH4HSO4", AMMONIUM + ".OS(=O)(=O)[O-]", "H5NO4S");
+        add(smilesByFormula, "NH4H2PO4", AMMONIUM + ".OP(=O)(O)[O-]", "H6NO4P");
+        add(smilesByFormula, "(NH4)2HPO4", AMMONIUM + "." + AMMONIUM + ".OP(=O)([O-])[O-]", "H9N2O4P");
     }
 
     private static void hydroxide(Map<String, String> smilesByFormula) {
