3509c6281f7fb75a61b13d35b0c2caf5a528b781
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-07 04:21:33 +0200

    refactor: convert ion match to record

--- ChemistryLab/src/main/java/org/chemistrylab/chemistry/ionic/IonMatch.java
+++ ChemistryLab/src/main/java/org/chemistrylab/chemistry/ionic/IonMatch.java
@@ -2,32 +2,5 @@ package org.chemistrylab.chemistry.ionic;
 
 import org.chemistrylab.chemistry.config.IonConfig;
 
-public class IonMatch {
-
-    private IonConfig ion;
-    private int cantidad;
-
-    public IonMatch() {
-    }
-
-    public IonMatch(IonConfig ion, int cantidad) {
-        this.ion = ion;
-        this.cantidad = cantidad;
-    }
-
-    public IonConfig getIon() {
-        return ion;
-    }
-
-    public void setIon(IonConfig ion) {
-        this.ion = ion;
-    }
-
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
+public record IonMatch(IonConfig ion, int cantidad) {
 }
