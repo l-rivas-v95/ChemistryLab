2fece68d0e9a4e4f8430a717e6c2b91e1cf5e760
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-08 00:16:41 +0200

    refactor: use curated smiles registry for oxide catalog

--- ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/OxideSmilesCatalog.java
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/OxideSmilesCatalog.java
@@ -2,35 +2,35 @@ package org.chemistrylab.representation.smiles;
 
 import java.util.Map;
 
-import static org.chemistrylab.representation.smiles.SmilesOverrideCatalog.put;
+import static org.chemistrylab.representation.smiles.CuratedSmilesRegistry.register;
 
 public final class OxideSmilesCatalog {
 
     private OxideSmilesCatalog() {
     }
 
-    public static void register(Map<String, String> overrides) {
-        covalentOxides(overrides);
-        metallicOxides(overrides);
+    public static void register(Map<String, String> smilesByFormula) {
+        covalentOxides(smilesByFormula);
+        metallicOxides(smilesByFormula);
     }
 
-    private static void covalentOxides(Map<String, String> overrides) {
-        put(overrides, "SO3", "O=S(=O)=O", "O3S");
-        put(overrides, "P2O5", "O=P(=O)OP(=O)=O", "O5P2");
-        put(overrides, "Cl2O", "ClOCl", "OCl2");
-        put(overrides, "Cl2O7", "O=Cl(=O)(=O)OCl(=O)(=O)=O", "O7Cl2");
+    private static void covalentOxides(Map<String, String> smilesByFormula) {
+        register(smilesByFormula, "SO3", "O=S(=O)=O", "O3S");
+        register(smilesByFormula, "P2O5", "O=P(=O)OP(=O)=O", "O5P2");
+        register(smilesByFormula, "Cl2O", "ClOCl", "OCl2");
+        register(smilesByFormula, "Cl2O7", "O=Cl(=O)(=O)OCl(=O)(=O)=O", "O7Cl2");
     }
 
-    private static void metallicOxides(Map<String, String> overrides) {
-        put(overrides, "Na2O", "[Na]O[Na]", "ONa2");
-        put(overrides, "K2O", "[K]O[K]", "OK2");
-        put(overrides, "CaO", "[Ca]=O", "OCa");
-        put(overrides, "MgO", "[Mg]=O", "OMg");
-        put(overrides, "FeO", "[Fe]=O", "OFe");
-        put(overrides, "CuO", "[Cu]=O", "OCu");
-        put(overrides, "ZnO", "[Zn]=O", "OZn");
-        put(overrides, "TiO2", "O=[Ti]=O", "O2Ti");
-        put(overrides, "Al2O3", "[O-2].[O-2].[Al+3].[Al+3].[O-2]", "O3Al2");
-        put(overrides, "Fe2O3", "[O-2].[O-2].[Fe+3].[Fe+3].[O-2]", "O3Fe2");
+    private static void metallicOxides(Map<String, String> smilesByFormula) {
+        register(smilesByFormula, "Na2O", "[Na]O[Na]", "ONa2");
+        register(smilesByFormula, "K2O", "[K]O[K]", "OK2");
+        register(smilesByFormula, "CaO", "[Ca]=O", "OCa");
+        register(smilesByFormula, "MgO", "[Mg]=O", "OMg");
+        register(smilesByFormula, "FeO", "[Fe]=O", "OFe");
+        register(smilesByFormula, "CuO", "[Cu]=O", "OCu");
+        register(smilesByFormula, "ZnO", "[Zn]=O", "OZn");
+        register(smilesByFormula, "TiO2", "O=[Ti]=O", "O2Ti");
+        register(smilesByFormula, "Al2O3", "[O-2].[O-2].[Al+3].[Al+3].[O-2]", "O3Al2");
+        register(smilesByFormula, "Fe2O3", "[O-2].[O-2].[Fe+3].[Fe+3].[O-2]", "O3Fe2");
     }
 }
