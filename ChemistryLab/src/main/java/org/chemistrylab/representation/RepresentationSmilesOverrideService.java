5f6039f4834ea77e579af71e79714db227c0aa3b
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-07 18:33:28 +0200

    feat: render group one and two hydroxides compactly

--- ChemistryLab/src/main/java/org/chemistrylab/representation/RepresentationSmilesOverrideService.java
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/RepresentationSmilesOverrideService.java
@@ -63,8 +63,15 @@ public class RepresentationSmilesOverrideService {
     }
 
     private static void hidroxidosSeleccionados(Map<String, String> overrides) {
-        put(overrides, "NaOH", "[H]O[Na]");
-        put(overrides, "NH4OH", "[NH4+].[OH-]");
+        put(overrides, "LiOH", "[H]O[Li]", "HLiO");
+        put(overrides, "NaOH", "[H]O[Na]", "HNaO");
+        put(overrides, "KOH", "[H]O[K]", "HKO");
+        put(overrides, "Mg(OH)2", "[H]O[Mg]O[H]", "H2MgO2");
+        put(overrides, "Ca(OH)2", "[H]O[Ca]O[H]", "CaH2O2");
+        put(overrides, "Ba(OH)2", "[H]O[Ba]O[H]", "BaH2O2");
+        put(overrides, "Al(OH)3", "O[Al](O)O", "AlH3O3");
+        put(overrides, "Fe(OH)3", "O[Fe](O)O", "FeH3O3");
+        put(overrides, "NH4OH", "[NH4+].[OH-]", "H5NO");
     }
 
     private static void salesBinariasUnoAUno(Map<String, String> overrides) {
