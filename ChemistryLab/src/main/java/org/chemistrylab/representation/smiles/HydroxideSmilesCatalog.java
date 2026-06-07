a6b7c381e586fb37c9f83e3f9547b30885bd4b5b
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-08 00:10:24 +0200

    refactor: use curated smiles registry for hydroxide catalog

--- ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/HydroxideSmilesCatalog.java
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/HydroxideSmilesCatalog.java
@@ -2,21 +2,21 @@ package org.chemistrylab.representation.smiles;
 
 import java.util.Map;
 
-import static org.chemistrylab.representation.smiles.SmilesOverrideCatalog.put;
+import static org.chemistrylab.representation.smiles.CuratedSmilesRegistry.register;
 
 public final class HydroxideSmilesCatalog {
 
     private HydroxideSmilesCatalog() {
     }
 
-    public static void register(Map<String, String> overrides) {
-        put(overrides, "LiOH", "[H]O[Li]", "HLiO");
-        put(overrides, "NaOH", "[H]O[Na]", "HNaO");
-        put(overrides, "KOH", "[H]O[K]", "HKO");
-        put(overrides, "Mg(OH)2", "[H]O[Mg]O[H]", "H2MgO2");
-        put(overrides, "Ca(OH)2", "[H]O[Ca]O[H]", "CaH2O2");
-        put(overrides, "Ba(OH)2", "[H]O[Ba]O[H]", "BaH2O2");
-        put(overrides, "Al(OH)3", "O[Al](O)O", "AlH3O3");
-        put(overrides, "Fe(OH)3", "O[Fe](O)O", "FeH3O3");
+    public static void register(Map<String, String> smilesByFormula) {
+        register(smilesByFormula, "LiOH", "[H]O[Li]", "HLiO");
+        register(smilesByFormula, "NaOH", "[H]O[Na]", "HNaO");
+        register(smilesByFormula, "KOH", "[H]O[K]", "HKO");
+        register(smilesByFormula, "Mg(OH)2", "[H]O[Mg]O[H]", "H2MgO2");
+        register(smilesByFormula, "Ca(OH)2", "[H]O[Ca]O[H]", "CaH2O2");
+        register(smilesByFormula, "Ba(OH)2", "[H]O[Ba]O[H]", "BaH2O2");
+        register(smilesByFormula, "Al(OH)3", "O[Al](O)O", "AlH3O3");
+        register(smilesByFormula, "Fe(OH)3", "O[Fe](O)O", "FeH3O3");
     }
 }
