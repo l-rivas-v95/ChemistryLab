16c62225978302d89a3ff432de003a17cebac08a
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-08 01:08:04 +0200

    Remove oxosalt aliases covered by formula normalization

--- ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/OxosaltSmilesCatalog.java
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/OxosaltSmilesCatalog.java
@@ -36,32 +36,32 @@ public final class OxosaltSmilesCatalog {
         add(smilesByFormula, "K2CO3", "[K+].[O-]C(=O)[O-].[K+]", "CK2O3");
         add(smilesByFormula, "MgCO3", "[O-]C(=O)[O-].[Mg+2]", "CMgO3");
         add(smilesByFormula, "CaCO3", "[O-]C(=O)[O-].[Ca+2]", "CCaO3");
-        add(smilesByFormula, "KHCO3", "OC(=O)[O-].[K+]", "CHKO3");
-        add(smilesByFormula, "NaHCO3", "OC(=O)[O-].[Na+]", "CHNaO3");
-        add(smilesByFormula, "Ca(HCO3)2", "OC(=O)[O-].[Ca+2].OC(=O)[O-]", "CaH2C2O6");
+        add(smilesByFormula, "KHCO3", "OC(=O)[O-].[K+]");
+        add(smilesByFormula, "NaHCO3", "OC(=O)[O-].[Na+]");
+        add(smilesByFormula, "Ca(HCO3)2", "OC(=O)[O-].[Ca+2].OC(=O)[O-]");
     }
 
     private static void sulfates(Map<String, String> smilesByFormula) {
-        add(smilesByFormula, "Na2SO4", "[Na+].[O-]S(=O)(=O)[O-].[Na+]", "Na2O4S");
-        add(smilesByFormula, "K2SO4", "[K+].[O-]S(=O)(=O)[O-].[K+]", "K2O4S");
-        add(smilesByFormula, "CaSO4", "[O-]S(=O)(=O)[O-].[Ca+2]", "CaO4S");
-        add(smilesByFormula, "MgSO4", "[O-]S(=O)(=O)[O-].[Mg+2]", "MgO4S");
-        add(smilesByFormula, "CuSO4", "[O-]S(=O)(=O)[O-].[Cu+2]", "CuO4S");
-        add(smilesByFormula, "ZnSO4", "[O-]S(=O)(=O)[O-].[Zn+2]", "O4SZn");
-        add(smilesByFormula, "FeSO4", "[O-]S(=O)(=O)[O-].[Fe+2]", "FeO4S");
+        add(smilesByFormula, "Na2SO4", "[Na+].[O-]S(=O)(=O)[O-].[Na+]");
+        add(smilesByFormula, "K2SO4", "[K+].[O-]S(=O)(=O)[O-].[K+]");
+        add(smilesByFormula, "CaSO4", "[O-]S(=O)(=O)[O-].[Ca+2]");
+        add(smilesByFormula, "MgSO4", "[O-]S(=O)(=O)[O-].[Mg+2]");
+        add(smilesByFormula, "CuSO4", "[O-]S(=O)(=O)[O-].[Cu+2]");
+        add(smilesByFormula, "ZnSO4", "[O-]S(=O)(=O)[O-].[Zn+2]");
+        add(smilesByFormula, "FeSO4", "[O-]S(=O)(=O)[O-].[Fe+2]");
         add(smilesByFormula, "Al2(SO4)3", "[O-]S(=O)(=O)[O-].[Al+3].[O-]S(=O)(=O)[O-].[Al+3].[O-]S(=O)(=O)[O-]");
     }
 
     private static void phosphates(Map<String, String> smilesByFormula) {
-        add(smilesByFormula, "Na3PO4", "[Na+].[O-]P(=O)([O-])[O-].[Na+].[Na+]", "Na3O4P");
-        add(smilesByFormula, "K3PO4", "[K+].[O-]P(=O)([O-])[O-].[K+].[K+]", "K3O4P");
+        add(smilesByFormula, "Na3PO4", "[Na+].[O-]P(=O)([O-])[O-].[Na+].[Na+]");
+        add(smilesByFormula, "K3PO4", "[K+].[O-]P(=O)([O-])[O-].[K+].[K+]");
         add(smilesByFormula, "Ca3(PO4)2", "[Ca+2].[O-]P(=O)([O-])[O-].[Ca+2].[O-]P(=O)([O-])[O-].[Ca+2]");
         add(smilesByFormula, "Mg3(PO4)2", "[Mg+2].[O-]P(=O)([O-])[O-].[Mg+2].[O-]P(=O)([O-])[O-].[Mg+2]");
-        add(smilesByFormula, "AlPO4", "[O-]P(=O)([O-])[O-].[Al+3]", "AlO4P");
-        add(smilesByFormula, "FePO4", "[O-]P(=O)([O-])[O-].[Fe+3]", "FeO4P");
-        add(smilesByFormula, "NaH2PO4", "OP(=O)(O)[O-].[Na+]", "H2NaO4P");
-        add(smilesByFormula, "Na2HPO4", "OP(=O)([O-])[O-].[Na+].[Na+]", "HNa2O4P");
-        add(smilesByFormula, "CaHPO4", "OP(=O)([O-])[O-].[Ca+2]", "CaHO4P");
+        add(smilesByFormula, "AlPO4", "[O-]P(=O)([O-])[O-].[Al+3]");
+        add(smilesByFormula, "FePO4", "[O-]P(=O)([O-])[O-].[Fe+3]");
+        add(smilesByFormula, "NaH2PO4", "OP(=O)(O)[O-].[Na+]");
+        add(smilesByFormula, "Na2HPO4", "OP(=O)([O-])[O-].[Na+].[Na+]");
+        add(smilesByFormula, "CaHPO4", "OP(=O)([O-])[O-].[Ca+2]");
     }
 
     private static void chlorineOxoanions(Map<String, String> smilesByFormula) {
