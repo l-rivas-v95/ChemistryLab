b3eb02b398db17a8458e28aa660ecbaa505b21a0
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-08 00:28:13 +0200

    fix: use curated smiles registry add in oxide catalog

--- ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/OxideSmilesCatalog.java
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/OxideSmilesCatalog.java
@@ -2,7 +2,7 @@ package org.chemistrylab.representation.smiles;
 
 import java.util.Map;
 
-import static org.chemistrylab.representation.smiles.CuratedSmilesRegistry.register;
+import static org.chemistrylab.representation.smiles.CuratedSmilesRegistry.add;
 
 public final class OxideSmilesCatalog {
 
@@ -15,22 +15,22 @@ public final class OxideSmilesCatalog {
     }
 
     private static void covalentOxides(Map<String, String> smilesByFormula) {
-        register(smilesByFormula, "SO3", "O=S(=O)=O", "O3S");
-        register(smilesByFormula, "P2O5", "O=P(=O)OP(=O)=O", "O5P2");
-        register(smilesByFormula, "Cl2O", "ClOCl", "OCl2");
-        register(smilesByFormula, "Cl2O7", "O=Cl(=O)(=O)OCl(=O)(=O)=O", "O7Cl2");
+        add(smilesByFormula, "SO3", "O=S(=O)=O", "O3S");
+        add(smilesByFormula, "P2O5", "O=P(=O)OP(=O)=O", "O5P2");
+        add(smilesByFormula, "Cl2O", "ClOCl", "OCl2");
+        add(smilesByFormula, "Cl2O7", "O=Cl(=O)(=O)OCl(=O)(=O)=O", "O7Cl2");
     }
 
     private static void metallicOxides(Map<String, String> smilesByFormula) {
-        register(smilesByFormula, "Na2O", "[Na]O[Na]", "ONa2");
-        register(smilesByFormula, "K2O", "[K]O[K]", "OK2");
-        register(smilesByFormula, "CaO", "[Ca]=O", "OCa");
-        register(smilesByFormula, "MgO", "[Mg]=O", "OMg");
-        register(smilesByFormula, "FeO", "[Fe]=O", "OFe");
-        register(smilesByFormula, "CuO", "[Cu]=O", "OCu");
-        register(smilesByFormula, "ZnO", "[Zn]=O", "OZn");
-        register(smilesByFormula, "TiO2", "O=[Ti]=O", "O2Ti");
-        register(smilesByFormula, "Al2O3", "[O-2].[O-2].[Al+3].[Al+3].[O-2]", "O3Al2");
-        register(smilesByFormula, "Fe2O3", "[O-2].[O-2].[Fe+3].[Fe+3].[O-2]", "O3Fe2");
+        add(smilesByFormula, "Na2O", "[Na]O[Na]", "ONa2");
+        add(smilesByFormula, "K2O", "[K]O[K]", "OK2");
+        add(smilesByFormula, "CaO", "[Ca]=O", "OCa");
+        add(smilesByFormula, "MgO", "[Mg]=O", "OMg");
+        add(smilesByFormula, "FeO", "[Fe]=O", "OFe");
+        add(smilesByFormula, "CuO", "[Cu]=O", "OCu");
+        add(smilesByFormula, "ZnO", "[Zn]=O", "OZn");
+        add(smilesByFormula, "TiO2", "O=[Ti]=O", "O2Ti");
+        add(smilesByFormula, "Al2O3", "[O-2].[O-2].[Al+3].[Al+3].[O-2]", "O3Al2");
+        add(smilesByFormula, "Fe2O3", "[O-2].[O-2].[Fe+3].[Fe+3].[O-2]", "O3Fe2");
     }
 }
