7849b1b03ae8d77f3f3a85fa9080ee1e396f2868
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-07 20:04:04 +0200

    refactor: remove unused external image representation factory

--- ChemistryLab/src/main/java/org/chemistrylab/dto/MoleculaRepresentacionDTO.java
+++ ChemistryLab/src/main/java/org/chemistrylab/dto/MoleculaRepresentacionDTO.java
@@ -24,16 +24,6 @@ public class MoleculaRepresentacionDTO {
         return dto;
     }
 
-    public static MoleculaRepresentacionDTO imagenExterna(String formulaVisual, String imagen2d, String reason) {
-        MoleculaRepresentacionDTO dto = new MoleculaRepresentacionDTO();
-        dto.setTipoRepresentacion("IMAGEN_2D");
-        dto.setFormulaVisual(formulaVisual);
-        dto.setImagen2d(imagen2d);
-        dto.setImagenRepresentacionSource("PUBCHEM_IMAGE_2D");
-        dto.setImagenRepresentacionReason(reason);
-        return dto;
-    }
-
     public static MoleculaRepresentacionDTO formula(String formulaVisual) {
         MoleculaRepresentacionDTO dto = new MoleculaRepresentacionDTO();
         dto.setTipoRepresentacion("FORMULA");
