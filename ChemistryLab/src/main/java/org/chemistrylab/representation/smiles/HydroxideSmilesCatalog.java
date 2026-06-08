f450a1f659bf6f67ef06fadc14cab07dea0fa215
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-08 13:15:44 +0200

    Remove alkaline earth hydroxides covered by ionic rule

--- ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/HydroxideSmilesCatalog.java
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/HydroxideSmilesCatalog.java
@@ -10,9 +10,6 @@ public final class HydroxideSmilesCatalog {
     }
 
     public static void register(Map<String, String> smilesByFormula) {
-        add(smilesByFormula, "Mg(OH)2", "[H]O[Mg]O[H]", "H2MgO2");
-        add(smilesByFormula, "Ca(OH)2", "[H]O[Ca]O[H]", "CaH2O2");
-        add(smilesByFormula, "Ba(OH)2", "[H]O[Ba]O[H]", "BaH2O2");
         add(smilesByFormula, "Al(OH)3", "O[Al](O)O", "AlH3O3");
         add(smilesByFormula, "Fe(OH)3", "O[Fe](O)O", "FeH3O3");
     }
