8f47f943a11268569d3007d05d27c6b5112337c8
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-08 22:36:19 +0200

    Add mixed oxide SMILES rule

---
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/MixedOxideSmilesRule.java
@@ -0,0 +1,29 @@
+package org.chemistrylab.representation;
+
+import org.chemistrylab.chemistry.formula.FormulaParserService;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class MixedOxideSmilesRule {
+
+    private final FormulaParserService formulaParserService;
+
+    public MixedOxideSmilesRule(FormulaParserService formulaParserService) {
+        this.formulaParserService = formulaParserService;
+    }
+
+    public Optional<String> build(String formula) {
+        if (formula == null || formula.isBlank()) {
+            return Optional.empty();
+        }
+
+        String normalizedFormula = formulaParserService.normalizarFormulaVisual(formula);
+        if ("Fe3O4".equals(normalizedFormula)) {
+            return Optional.of("[O-2].[Fe+3].[O-2].[Fe+2].[O-2].[Fe+3].[O-2]");
+        }
+
+        return Optional.empty();
+    }
+}
