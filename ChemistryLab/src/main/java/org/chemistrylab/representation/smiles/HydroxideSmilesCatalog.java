4c65c46782ed0fdfa19f3b74c4a4a1bcd5959924
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-08 13:30:58 +0200

    Empty hydroxide catalog covered by rules

--- ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/HydroxideSmilesCatalog.java
+++ ChemistryLab/src/main/java/org/chemistrylab/representation/smiles/HydroxideSmilesCatalog.java
@@ -2,15 +2,13 @@ package org.chemistrylab.representation.smiles;
 
 import java.util.Map;
 
-import static org.chemistrylab.representation.smiles.CuratedSmilesRegistry.add;
-
 public final class HydroxideSmilesCatalog {
 
     private HydroxideSmilesCatalog() {
     }
 
     public static void register(Map<String, String> smilesByFormula) {
-        add(smilesByFormula, "Al(OH)3", "O[Al](O)O", "AlH3O3");
-        add(smilesByFormula, "Fe(OH)3", "O[Fe](O)O", "FeH3O3");
+        // Hidroxidos metalicos cubiertos por reglas de representacion.
+        // Mantener esta clase como punto de excepciones visuales si aparece algun caso especial.
     }
 }
